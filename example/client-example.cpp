d7335f152999b085f0d92a1afe68c4c4e7123336
LJ3D <65134690+LJ3D@users.noreply.github.com>
2023-05-01 14:41:06 +0100

    separate client+service examples

--- example/client-example.cpp
+++ example/client-example.cpp
@@ -6,9 +6,9 @@
 #define HOST "cryptbbtg65gibadeeo2awe3j7s6evg7eklserehqr4w4e2bis5tebid.onion"
 
 int main(){
-    torPlusPlus::TOR tor(9051, ".tpptorrc", true, "./tor"); // port 9050, create config file at "./tpptorrc", enable logging, and use "./tor" as the path to the tor binary
+    torPlusPlus::TOR tor(9051, ".tpptorrc", true); // port 9050, create config file at "./tpptorrc", enable logging, and use "./tor" as the path to the tor binary
     // NOTE: The config file will be "cleared" (deleted), so dont accidentally set the path to anything important!
-    tor.startFromFile(); // Start the tor binary
+    tor.start(); // Start the tor binary
     torPlusPlus::TORSocket ts = tor.getSocket(); // Get a pre-configured socket that is already connected to the "tor" instance
     ts.connectProxyTo(HOST, 80); // Connect to the hidden service
     std::string httpReq = "GET / HTTP/1.1\r\nHost: " + std::string(HOST) + "\r\n\r\n"; // Assemble a HTTP GET request to send to the site
@@ -18,14 +18,5 @@ int main(){
     printf("%s\n", buf); // Print whatever the server sent back
     free(buf);
     tor.stop(); // This happens when the class is destroyed, but you can do it manually too :)
-
-
-    // Create a service that listens on port 80 and forwards all traffic to port 8081
-    tor.addService("./testService", 80, 8081);
-    tor.startFromFile();
-
-    // Run TOR for 60 seconds
-    sleep(60);
-
     return 0;
 }
---
+++ example/service-example.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <thread>
+
+#include "../torplusplus.hpp"
+
+#define HOST "cryptbbtg65gibadeeo2awe3j7s6evg7eklserehqr4w4e2bis5tebid.onion"
+
+void clientRecvThread(int sock){
+    while(true){
+        char buf[1024];
+        int len = recv(sock, buf, 50000, 0);
+        if(len <= 0){
+            printf("=== Client disconnected\n");
+            break;
+        }
+        printf("%s", buf);
+    }
+}
+
+int main(){
+    torPlusPlus::TOR tor(9051, ".tpptorrc", true); // port 9050, create config file at "./tpptorrc", enable logging, and use "./tor" as the path to the tor binary
+    // NOTE: The config file will be "cleared" (deleted), so dont accidentally set the path to anything important!
+    // Create a service that listens on port 80 and forwards all traffic to port 8081
+    tor.addService("./testService", 80, 8081);
+    tor.start();
+
+    // Create a socket that listens on port 8081 and prints anything it receives to stdout
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(8081);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    bind(sock, (struct sockaddr*)&addr, sizeof(addr));
+    listen(sock, 5);
+    while(true){
+        int clientSock = accept(sock, NULL, NULL);
+        printf("=== Client connected\n");
+        std::thread t(clientRecvThread, clientSock);
+        t.detach();
+    }
+    return 0;
+}
